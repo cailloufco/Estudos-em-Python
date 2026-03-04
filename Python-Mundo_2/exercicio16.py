02fdee812d358d57b66ece44df6017abe64461c3
Caio <caioextreme000@gmail.com>
2026-03-04 11:36:20 -0300

    Update exercicio16.py

--- Python-Mundo_2/exercicio16.py
+++ Python-Mundo_2/exercicio16.py
@@ -1,3 +1,14 @@
 #Faça um programa que leia o peso de cinco pessoas. No final , mostre qual foi o maior e o menor peso lidos.
 
-lista = []
+lista_de_pesos = []
+
+for contagem in range (1 , 6):
+    lista_de_pesos.append(int(input(f'{contagem} - Digite o peso de CINCO pessoas em KG: ')))
+
+lista_de_pesos.sort()
+
+print(f'o menor peso foi {min(lista_de_pesos)} e o maior foi {max(lista_de_pesos)}')
+
+
+
+
