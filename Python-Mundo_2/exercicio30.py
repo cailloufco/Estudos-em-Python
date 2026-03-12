2a1fd29e95a8f3936c438dc2380a167e87d156fe
Caio <caioextreme000@gmail.com>
2026-03-12 16:03:24 -0300

    Update exercicio30.py

--- Python-Mundo_2/exercicio30.py
+++ Python-Mundo_2/exercicio30.py
@@ -1,27 +1,25 @@
 # Crie um programa que simule o funcionamento de um caixa eletrônico. No início , pergunte ao usuário qual será o valor a ser sacado (NUMERO INTEIRO) e o programa vai informar quantas cédulas de cada valor serão entregues
 # OBS: Considere que o vaixa possui cédulas de R$50 , R$20 , R$10 e R$1
 
-valor_sacado = int(input('Digite o valor que será sacado'))
-resto = None
+valor_sacado = int(input('Digite o valor que será sacado: '))
 while True:
     if valor_sacado > 0:
         if valor_sacado >= 50:
             print(f'NOTAS DE 50 = {valor_sacado//50}')
-            resto = valor_sacado % 50
-            if resto == 0:
+            valor_sacado = valor_sacado % 50
+            if valor_sacado == 0:
                 break
-        if resto >= 20:
-            print(f'NOTAS DE 20 = {resto//20}')
-            resto = resto % 20
-            if resto == 0:
+        if valor_sacado >= 20:
+            print(f'NOTAS DE 20 = {valor_sacado//20}')
+            valor_sacado = valor_sacado % 20
+            if valor_sacado == 0:
                 break
-        if resto >= 10:
-            print(f'NOTAS DE 10 = {resto//10}')
-            if resto == 0:
+        if valor_sacado >= 10:
+            print(f'NOTAS DE 10 = {valor_sacado//10}')
+            if valor_sacado == 0:
                 break
-        if resto >= 1:
-            print(f'MOEDAS DE 1 = {resto}')
-            
+        if valor_sacado >= 1:
+            print(f'MOEDAS DE 1 = {valor_sacado}')            
     else:
         print('Valor inválido')
     break
