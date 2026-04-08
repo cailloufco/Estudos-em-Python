1f4ac2b42f0f773ace58dc2d8a34fb0d91948280
Caio <caioextreme000@gmail.com>
2026-04-08 11:24:34 -0300

    Update exercicio7.py

--- Python-Mundo_3/exercicio7.py
+++ Python-Mundo_3/exercicio7.py
@@ -4,7 +4,7 @@
 valores_num = []
 
 for numero in range(5):
-    numero = float(input('Digite um valor númerico!: '))
+    numero = float(input(f'Digite um valor númerico!: {numero}º '))
     valores_num.append(numero)
 
-print(f'O maior valor listado foi {max(valores_num)} , na posição {valores_num.index(max(valores_num))}\nO menor valor listado foi {min(valores_num)} , na posição {valores_num.index(min(valores_num))}')
+print(f'Os valores digitador foram {' , '.join(map(str , valores_num))}\nO maior valor listado foi {max(valores_num)} , na posição {valores_num.index(max(valores_num))}\nO menor valor listado foi {min(valores_num)} , na posição {valores_num.index(min(valores_num))}')
